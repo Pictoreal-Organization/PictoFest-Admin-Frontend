89a0843adc11229a79d7b53d598a6b9497bb0123
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2024-02-16 17:29:34 +0530

    Home Page Dummy Text Added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,12 @@
 "use client";
 
 const Home = () => {
-  return <main className="">Home</main>;
+  return (
+    <main className="flex flex-col justify-center items-center w-4/5 gap-5">
+      <h1 className="text-4xl">Welcome To Dashboard</h1>
+      <h3 className="text-3xl">Explore Sidebar</h3>
+    </main>
+  );
 };
 
 export default Home;
